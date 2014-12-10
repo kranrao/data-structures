ceeea31b6cb0d3eae27c91baefe57c4c483b9464
Kiran Rao <k.ranrao@gmail.com>
2014-12-10 12:28:10 -0800

    Solved functional stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,36 @@ var Stack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  // Track number of numeric keys in object
+  var counter = 0;
+
   // Implement the methods below
   someInstance.push = function(value){
+    // Add value to top of stack
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function(){
+    var poppedValue = storage[counter];    
+
+    if (counter <= 0) {
+      counter = 0;
+    } else {
+      // Delete value at the top of the stack
+      delete storage[counter];
+
+      // Reduce counter so that next push overwrites popped key 
+      counter--;
+
+      // Return value at top of the stack
+      return poppedValue;  
+    }
   };
 
   someInstance.size = function(){
+    // Return the number of items on the stack
+    return counter;
   };
 
   return someInstance;
