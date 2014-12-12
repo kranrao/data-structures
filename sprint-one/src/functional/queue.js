c1edc7a88029f76c895bb7297074205496346f8c
Kiran Rao <k.ranrao@gmail.com>
2014-12-11 21:17:26 -0800

    Adjusted merge branching error for functional constructor

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,1 +1,34 @@
-data_structure_new update is more relevant
+var Queue = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  var storage = {};
+  var counter = 0;
+  
+  someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter++;
+  };
+
+  someInstance.dequeue = function() {
+    var result = storage[0];
+    delete storage[0];
+    counter--;
+
+    if (counter < 0) {
+      counter = 0;
+    }
+
+    for (var i = counter; i > 0; i--) {
+      storage[i - 1] = storage[i]
+    }
+
+    return result
+  };
+
+  someInstance.size = function() {
+    return counter;
+  };
+
+  return someInstance;
+};
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,1 +1,30 @@
-Use data_structure_new edits
+var Stack = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  var storage = {};
+  var counter = 0;
+  
+  someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
+  };
+
+  someInstance.pop = function() {
+    var result = storage[counter - 1];
+    delete storage[counter - 1];
+    counter--;
+
+    if (counter < 0) {
+      counter = 0;
+    }
+
+    return result
+  };
+
+  someInstance.size = function() {
+    return counter;
+  };
+
+  return someInstance;
+};
