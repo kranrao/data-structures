20a0c048b25fde2f77a024c46cb2da574f2ca299
Kiran Rao <k.ranrao@gmail.com>
2014-12-11 20:49:56 -0800

    Solved functional clases for queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function(){
+    var result = storage[0];
+    delete storage[0];
+    counter--;
+    if (counter < 0) {
+      counter = 0;
+    }
+    for (var i = counter; i > 0; i--) {
+      storage[i - 1] = storage[i];
+    }
+    return result;
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
