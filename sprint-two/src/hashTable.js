19ccf0da6c07147b2408ac36738e389f8009cd2e
kranrao <k.ranrao@gmail.com>
2014-12-12 21:03:47 -0800

    Adds pseudo code for retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,6 +5,18 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  // create new hashtable bucketHash
+  // create bucket property on bucketHash._storage
+  // bucket property is an array
+  // bucket property index randomized by key
+
+  // create new hashtable tupleHash
+  // create tuple property on tupleHash._storage
+  // tuple property is an array
+  // array [key, value]
+  // tuple property index randomized by value
+
 };
 
 HashTable.prototype.retrieve = function(k){
