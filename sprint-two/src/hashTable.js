595330e65f14d80040acd34bc6948f7b7b1c11ee
kranrao <k.ranrao@gmail.com>
2014-12-12 21:38:38 -0800

    Completed insert and retrieve methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,6 +3,8 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+// created new subhashtables for bucket and tuple in order
+// to use get, set, and each methods
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
@@ -21,11 +23,14 @@ HashTable.prototype.insert = function(k, v){
   // array [key, value]
   tupleHash._storage = [k, v];
   // tuple property index randomized by value
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  // did not use get
+  return this.bucketHash._storage.bucket[i]._storage[1];
 };
 
 HashTable.prototype.remove = function(k){
