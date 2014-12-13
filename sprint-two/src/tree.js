b26fc4315e5f0e309e66671115389387823d8f6d
kranrao <k.ranrao@gmail.com>
2014-12-12 16:23:48 -0800

    Solved tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,7 @@
 var Tree = function(value){
   var newTree = Object.create(treeMethods);
   newTree.value = value;
-
-
-  newTree.children = [];  // fix me
-
+  newTree.children = [];
   return newTree;
 };
 
@@ -21,20 +18,22 @@ treeMethods.addChild = function(value){
 treeMethods.contains = function(target){
   // global variable always false, not changing
   var hasTarget = false;
-  console.log(this.value, target, hasTarget)
   //check if the current node.value === target
-  if(this.value === target){
-    console.log('sss')
-    hasTarget = true;
-  } else{
-    if(this.children[0]){
-  //if false, pass the current node's children into .contains
-      for(var i = 0; i < this.children.length; i++){
- //       console.log("length",this.children.length, this.children, i)
-        this.children[i].contains(target)
+
+  //subroutine to check nodes
+  var checkNodes = function(node) {
+    if(node.value === target){
+      hasTarget = true;
+    } else{
+      if(node.children[0]){
+        for(var i = 0; i < node.children.length; i++){
+          checkNodes(node.children[i]);
+        }
       }
     }
-  }
+  };
+
+  checkNodes(this);
 
   return hasTarget;
 };
