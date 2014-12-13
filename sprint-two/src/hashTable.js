6edb51ebd13a662cc6a464fcdd69ffb9b8802ee6
kranrao <k.ranrao@gmail.com>
2014-12-12 21:28:56 -0800

    Some code for insert

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,16 +7,20 @@ HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
   // create new hashtable bucketHash
+  this.bucketHash = new HashTable ();
   // create bucket property on bucketHash._storage
   // bucket property is an array
+  this.bucketHash._storage.bucket = []
   // bucket property index randomized by key
 
   // create new hashtable tupleHash
+  var tupleHash = new HashTable ();
+  this.bucketHash._storage.bucket[i] = tupleHash;
   // create tuple property on tupleHash._storage
   // tuple property is an array
   // array [key, value]
+  tupleHash._storage = [k, v];
   // tuple property index randomized by value
-
 };
 
 HashTable.prototype.retrieve = function(k){
