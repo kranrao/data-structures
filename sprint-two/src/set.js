f064289bfd9b6a8aceeb381b70615ee332251d91
kranrao <k.ranrao@gmail.com>
2014-12-13 00:14:17 -0800

    Solved set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item);
+
+  //attemp to fill in the first available open position in the array
+/*  for (var i = 0; i < this._storage.length; i++) {
+    this._storage[i] === undefined ? this.storage[i] = item : this._storage.push(item);
+  }*/
 };
 
 setPrototype.contains = function(item){
+  var hasItem = false;
+  for (var i = 0; i < this._storage.length; i++) {
+    this._storage[i] === item ? hasItem = true : null;
+  }
+  return hasItem;
 };
 
 setPrototype.remove = function(item){
+  for (var i = 0; i < this._storage.length; i++) {
+    this._storage[i] === item ? delete this._storage[i] : null;
+  }
 };
 
 /*
